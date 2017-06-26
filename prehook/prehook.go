ad1a7a237e8aae995b8fc31c9272d822b4aa088e
Ambarish <ambarish.a@flipkart.com>
2017-06-27 02:36:28 +0530

    removed token

--- prehook/prehook.go
+++ prehook/prehook.go
@@ -12,6 +12,6 @@ func AutoUpdate()  {
 }
 
 func downloadFromGithub(){
-  //curl -LJO# -H 'Accept: application/octet-stream' "https://api.github.com/repos/Flipkart/artifactory-service/releases/assets/4167867?access_token=3dbc5aaf29eecdfed24acc76496b8433a839805c"
+  //curl -LJO# -H 'Accept: application/octet-stream' "https://api.github.com/repos/Flipkart/artifactory-service/releases/assets/4167867?access_token=<token>"
   //make a get call to this url
 }
